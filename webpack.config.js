58f021f31f96dabac96c84cbf11b5434b0b7a1d5
psdhtmlcss <psdhtmlcss@mail.ru>
2022-07-20 23:51:00 +0400

    html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,30 +3,6 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const NunjucksWebpackPlugin = require('nunjucks-webpack-plugin');
-const glob_entries = require('webpack-glob-folder-entries');
-
-//#1: Define the HTML Webpack Plugin:
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  filename: 'index.html',
-  inject: 'body',
-
-  // Here is part of the magic, we get the index.njk but we tell
-  // webpack to pass it through the nunjucks-html-loader
-  template: 'nunjucks-html-loader!./src/pages/index.njk',
-});
-
-// Optional, but highly recommended. Create a returnEntries:
-// Webpack doesn't support glob paths. For the nunjucks-html-loader
-// we need each path to be specified for it to work (YES, even subdirectories!)
-
-function returnEntries(globPath) {
-  let entries = glob_entries(globPath, true);
-  let folderList = new Array();
-  for (let folder in entries) {
-    folderList.push(path.join(__dirname, entries[folder]));
-  }
-  return folderList;
-}
 
 
 module.exports = {
@@ -53,15 +29,14 @@ module.exports = {
     watchFiles: ['src/**/*.html']
   },
   plugins: [
-    HtmlWebpackPluginConfig,
-    // new HtmlWebpackPlugin({
-    //   template: './src/pages/index.html',
-    //   filename: 'index.html'
-    // }),
-    // new HtmlWebpackPlugin({
-    //   template: './src/pages/about.html',
-    //   filename: 'about.html'
-    // }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/index.html',
+      filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/about.html',
+      filename: 'about.html'
+    }),
     // new NunjucksWebpackPlugin({
     //   templates: [
     //     {
@@ -100,23 +75,6 @@ module.exports = {
       //   test: /\.njk|nunjucks$/,
       //   use: ['nunjucks-loader'],
       // },
-      {
-        // HTML LOADER
-        // Super important: We need to test for the html 
-        // as well as the nunjucks files
-        test: /\.html$|njk|nunjucks/,
-        use: ['html-loader', {
-          loader: 'nunjucks-html-loader',
-          options: {
-            // Other super important. This will be the base
-            // directory in which webpack is going to find 
-            // the layout and any other file index.njk is calling.
-            searchPaths: [...returnEntries('./src/pages/**/')]
-            // Use the one below if you want to use a single path.
-            // searchPaths: ['./client/templates'],
-          }
-        }],
-      }
     ],
   }
 }
